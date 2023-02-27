e4e9936c9897cde5f56baf8b7a87fa1eb9024517
markpakingan <116990353+markpakingan@users.noreply.github.com>
2023-02-27 07:08:54 -0800

    add original files

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 
 from flask import Flask, request, render_template,redirect, flash, session
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -9,7 +9,7 @@ app.app_context().push()
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCH EMY_ECHO'] = True
 app.config['SECRET_KEY'] = "SECRET!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
@@ -21,7 +21,8 @@ db.create_all()
 
 @app.route('/')
 def home_page():
-    return redirect("/users")
+    posts = Post.query.order_by(Post.created_at.desc()).limit(5).all()
+    return render_template("posts/homepage.html", posts=posts)
 
 
 ##############################################################################
@@ -30,13 +31,13 @@ def home_page():
 def show_all_users():
     """Show all users"""
     users = User.query.order_by(User.last_name, User.first_name).all()
-    return render_template('index.html', users = users)
+    return render_template('users/index.html', users = users)
 
 
 @app.route('/users/new', methods = ["GET"])
 def show_user_form():
     """Show the user form"""
-    return render_template('user_form.html')
+    return render_template('users/new_user.html')
 
 
 @app.route('/users/new', methods = ["POST"])
@@ -57,7 +58,7 @@ def add_new_users():
 @app.route('/users/<int:user_id>')
 def show_unique_user(user_id):
     user = User.query.get_or_404(user_id)
-    return render_template('edit_user.html', user=user)
+    return render_template('users/show_user.html', user=user)
 
 
 @app.route('/users/<int:user_id>/edit')
@@ -65,7 +66,7 @@ def edit_unique_user(user_id):
     """Show a form to edit an existing user"""
 
     user = User.query.get_or_404(user_id)
-    return render_template('edit_user.html', user=user)
+    return render_template('users/edit_user.html', user=user)
 
 
 
@@ -91,3 +92,77 @@ def delete_unique_user(user_id):
     db.session.commit()
 
     return redirect("/users")
+
+##############################################################################
+ 
+
+@app.route('/users/<int:user_id>/posts/new')
+def show_post_form(user_id):
+    """Show form to add a post for that user."""
+
+    user = User.query.get_or_404(user_id)
+    return render_template('posts/add_post.html', user=user)
+
+
+
+@app.route('/users/<int:user_id>/posts/new', methods = ["POST"] )
+def add_new_post(user_id):
+    """Get Data via From for a Specific User."""
+
+    user = User.query.get_or_404(user_id)
+    new_post = Post(
+        title = request.form["title"],
+        content = request.form["content"],
+        # user = user
+        users_id = user_id
+        )
+
+
+    db.session.add(new_post)
+    db.session.commit()
+    # flash(f"Post '{new_post.title}' added.")
+
+    return redirect(f"/users/{user_id}")
+
+
+@app.route('/posts/<int:post_id>')
+def show_post(post_id):
+    """Show, edit , delete posts"""
+
+    post = Post.query.get_or_404(post_id)
+    return render_template('posts/show_post.html', post = post)
+
+
+@app.route('/posts/<int:post_id>/edit', methods = ["GET"])
+def show_edit_post(post_id):
+    """Show a form to edit an existing post"""
+
+    post = Post.query.get_or_404(post_id)
+    user = post.user
+    return render_template('posts/edit_post.html', post = post, user = user)
+
+
+@app.route('/posts/<int:post_id>/edit', methods = ["POST"])
+def edit_post(post_id):
+    """Handle editing of a post. Redirect back to the post view."""
+
+    post = Post.query.get_or_404(post_id)
+    post.title = request.form['title']
+    post.content = request.form['content']
+
+    db.session.add(post)
+    db.session.commit()
+    
+    return redirect(f"/users/{post.users_id}")
+
+
+
+@app.route ('/posts/<int:post_id>/delete', methods = ["POST"])
+def delete_post(post_id):
+
+    post = Post.query.get_or_404(post_id)
+
+    db.session.delete(post)
+    db.session.commit()
+
+    return redirect(f"/users/{post.users_id}")
--- models.py
+++ models.py
@@ -1,19 +1,27 @@
 """Models for Blogly."""
 
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
+DEFAULT_IMAGE_URL = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png"
 
-    
+
 class User (db.Model):
 
     __tablename__ = "users"
 
-    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    id = db.Column(db.Integer, primary_key = True)
     first_name = db.Column(db.String(999), nullable = False)
     last_name = db.Column(db.String(999), nullable = False)
-    image_url = db.Column(db.String(800), nullable = False)
+    image_url = db.Column(db.String(800), nullable = False, default = DEFAULT_IMAGE_URL)
+
+
+    posts = db.relationship("Post", backref="user", 
+                            # cascade="all, delete-orphan"
+                            )
+
 
     @property
     def full_name(self):
@@ -22,9 +30,28 @@ class User (db.Model):
         return f"{self.first_name} {self.last_name}"
 
 
+class Post (db.Model):
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(999), nullable = False)
+    content = db.Column(db.String(999), nullable = False)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    users_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = True)
+
+    @property
+    def friendly_date(self):
+        """Return nicely-formatted date."""
+
+        return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
+
+
 def connect_db(app):
     """Connect to database."""
 
     db.app = app
     db.init_app(app)
+
+
 
